b3633b123cca40a8a0c92bca737bd20c76e233e7
KayJuno <dangdan2807@gmail.com>
2022-09-03 11:26:54 +0700

    update: test event jenkins

--- src/server.js
+++ src/server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const POST = process.env.POST || 3000;
 
-app.get('/', (req, res) => {
-    res.send('welcome');
-});
-
 app.get('/contract', (req, res) => {
     res.send('contract');
+});
+app.get('/', (req, res) => {
+    res.send('welcome');
 });
 
 app.listen(POST, () => {
